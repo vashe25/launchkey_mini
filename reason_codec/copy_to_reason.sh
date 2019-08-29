a7d134cd10a2c9837b204aac168c2097ce6bd523
vshevchenko <vshevchenko@dalee.ru>
2019-08-30 01:55:04 +0300

    Added Propellerheads Reason remote codec

---
+++ reason_codec/copy_to_reason.sh
@@ -0,0 +1,3 @@
+cp -fi ./*.remotemap "/Library/Application Support/Propellerhead Software/Remote/Maps/Novation/"
+cp -fi ./*.luacodec "/Library/Application Support/Propellerhead Software/Remote/Codecs/Lua Codecs/Novation/"
+cp -fi ./*.lua "/Library/Application Support/Propellerhead Software/Remote/Codecs/Lua Codecs/Novation/"
